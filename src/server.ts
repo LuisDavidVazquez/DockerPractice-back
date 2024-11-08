cca9d5803e7924b962a3feb123a2782d01bb9914
Luis David <147574256+LuisDavidVazquez@users.noreply.github.com>
2024-11-07 18:49:31 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { Signale } from "signale";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 import { paymentRouter } from "./payment/infrastructure/PaymentRouter";
 import { initializePaymentTable } from "./database/mysql";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; 
 const app = express();
 const signale = new Signale();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API de pagos");
+});
+
+// Ruta de pagos
 app.use("/payments", paymentRouter);
 
 initializePaymentTable().then(() => {
